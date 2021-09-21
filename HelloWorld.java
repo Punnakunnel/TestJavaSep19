e2f36cb88cedea2b05e7590b66a561dd45cb142a
Punnakunnel <90509445+Punnakunnel@users.noreply.github.com>
2021-09-21 23:22:44 +0530

    Updated helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 public static void main(String[] args) {
-System.out.println("Hello Hi Welcome to the Jenkins world!");
+System.out.println("Welcome to the Jenkins world!");
 }
 }
